e8c2e3bc9ef0e6e63b87d215f70263a3a6ebfb6d
Mark Burnett <mark.m.burnett@gmail.com>
2014-09-29 14:00:20 -0500

    whitespace

--- manifests/workflow/web.pp
+++ manifests/workflow/web.pp
@@ -10,12 +10,12 @@ class ptero::workflow::web {
     app         => 'puppet:///modules/ptero/workflow/app.py',
 
     environment => {
-      'PTERO_PETRI_HOST' => $ptero::params::petri::host,
-      'PTERO_PETRI_PORT' => $ptero::params::petri::port,
+      'PTERO_PETRI_HOST'         => $ptero::params::petri::host,
+      'PTERO_PETRI_PORT'         => $ptero::params::petri::port,
       'PTERO_SHELL_COMMAND_HOST' => $ptero::params::shell_command::host,
       'PTERO_SHELL_COMMAND_PORT' => $ptero::params::shell_command::port,
-      'PTERO_WORKFLOW_HOST' => $ptero::params::workflow::host,
-      'PTERO_WORKFLOW_PORT' => $ptero::params::workflow::port,
+      'PTERO_WORKFLOW_HOST'      => $ptero::params::workflow::host,
+      'PTERO_WORKFLOW_PORT'      => $ptero::params::workflow::port,
       'PTERO_WORKFLOW_DB_STRING' => $ptero::params::workflow::database_url,
     },
   }
