9d7cf9cd6682bea5527508226b0b6711d880f756
Mark Burnett <mark.m.burnett@gmail.com>
2014-10-08 08:31:54 -0500

    setup celery for fork and http queues

--- manifests/shell_command/celery.pp
+++ manifests/shell_command/celery.pp
@@ -1,11 +1,25 @@
 class ptero::shell_command::celery {
   require ptero::params
 
-  ptero::celery{'shell-command':
+  ptero::celery{'shell-command-fork':
     code_dir => $ptero::params::shell_command::target_directory,
     source   => $ptero::params::shell_command::repo,
     revision => $ptero::params::shell_command::tag,
     app      => 'ptero_shell_command.implementation.celery_app:app',
+    queue    => 'fork',
+    environment => {
+      'CELERY_BROKER_URL'     => $ptero::params::shell_command::rabbitmq_url,
+      'CELERY_RESULT_BACKEND' => $ptero::params::shell_command::redis_url,
+      'PYTHONPATH'            => $ptero::params::shell_command::target_directory,
+    },
+  }
+
+  ptero::celery{'shell-command-http':
+    code_dir => $ptero::params::shell_command::target_directory,
+    source   => $ptero::params::shell_command::repo,
+    revision => $ptero::params::shell_command::tag,
+    app      => 'ptero_shell_command.implementation.celery_app:app',
+    queue    => 'http',
     environment => {
       'CELERY_BROKER_URL'     => $ptero::params::shell_command::rabbitmq_url,
       'CELERY_RESULT_BACKEND' => $ptero::params::shell_command::redis_url,
